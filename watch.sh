7f728377b334b5b42f60f2bd1920caccef64f935
Omar Chehab <omarchehab98@gmail.com>
2018-11-30 20:33:42 -0500

    Update pom.xml and watch.sh

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 npm install -g nodemon
-nodemon -e java -x 'mvn package && java -cp target/cscc43-assignment-1.0-SNAPSHOT.jar cscc43.assignment.App'
+nodemon -e java -x 'mvn package && java -jar target/cscc43-assignment-1.0-SNAPSHOT.jar'
